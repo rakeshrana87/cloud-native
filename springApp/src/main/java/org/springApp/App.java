e43ddb613d17b1ccc17b6d0020d962118a99dd28
rakesh rana <er.rakeshrana@gmail.com>
2021-03-18 10:41:12 +0530

    added code to springapp module

---
+++ springApp/src/main/java/org/springApp/App.java
@@ -0,0 +1,13 @@
+package org.springApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
